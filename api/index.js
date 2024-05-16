5a8a75d2d2ffbe5a56c72d6c847c548174f7d549
dnayak <debendranayak2013@gmail.com>
2024-05-16 10:15:24 +0530

    added auth, error middleware

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,8 +12,20 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 app.listen(3001, () => {
     console.log('server is running in port 3000..');
 })
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statuscode = err.statuscode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statuscode).json({
+        success : false,
+        statuscode,
+        message,
+    })
+})
